d1b8489e8f16215599c5d9ce85e93dba47f39fa0
Tisha kharade <kharadetisha@gmail.com>
2025-04-23 13:07:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2>Hostel Ess Tiffin Service</h2>
+    <img src="https://assets.gqindia.com/photos/5e13119f28dff200080a598d/16:9/w_2560%2Cc_limit/top-image.jpg"/>
+     
+    </>
+  )
+}
+
+export default App
